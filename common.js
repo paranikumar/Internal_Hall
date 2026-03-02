71d09a680665f14aa33d2da9dfd12fb8878e2b7f
paranikumar <paranikumar11711@gmail.com>
2026-03-02 12:30:45 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbxfjm2aHUf-pu5d-Av1HKnnAxN4p8lKc4UBhGejqH47y05yc6HRDoiyO_hwLGSHypSu0g/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbydW16Dg3_MnmwXMAG0OAQkgONKzsuHe3kYf_XZ208NBEFU6ha9mz__wVzG-E_lXSKG/exec";
 
 async function apiFetch(payload) {
   // payload is an object with at least `action` property
@@ -30,6 +30,7 @@ function logout() {
   localStorage.removeItem("user");
   window.location.href = "login.html";
 }
+
 
 
 
